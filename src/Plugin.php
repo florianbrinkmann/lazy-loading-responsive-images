f553514c5f2186301c0c5c9c5eb788532edc5394
Florian Brinkmann <kontakt@florianbrinkmann.com>
2018-03-07 13:31:05 +0100

    Included license of loading spinner into LICENSE

--- src/Plugin.php
+++ src/Plugin.php
@@ -561,7 +561,7 @@ class Plugin {
 		 */
 		echo apply_filters( 'lazy_load_responsive_images_inline_styles', $default_styles );
 
-		// Display the spinner style if needed.
+		// Display the loading spinner style if needed.
 		$spinner_color  = $this->settings->loading_spinner_color;
 		$spinner_markup = sprintf(
 			'<svg width="44" height="44" xmlns="http://www.w3.org/2000/svg" stroke="%s"><g fill="none" fill-rule="evenodd" stroke-width="2"><circle cx="22" cy="22" r="1"><animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/><animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/></circle><circle cx="22" cy="22" r="1"><animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/><animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/></circle></g></svg>',
